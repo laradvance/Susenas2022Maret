3ed75f6b7ac17b95797dd39c70181b51af622bd3
Laradvance <35951442+laradvance@users.noreply.github.com>
2022-03-05 07:28:36 +0800

    Add files via upload

--- sekolah.js
+++ sekolah.js
@@ -12,8 +12,7 @@ new Vue({
       sma1: 0,
       sd2: 0,
       smp2:0,
-      sma2: 0,
-         
+      sma2: 0,         
     };
   },
   methods: {
@@ -25,26 +24,25 @@ new Vue({
         return amount;
     }
     },
-
-    r265() {
-      return 0;
-
-    },
+    r265(){
+      return (100000 * this.sd1 + 0 * this.sd2) +
+      (100000 * this.smp1 + 0 * this.smp2) +
+      (100000 * this.sma1 + 0 * this.sma2) 
+    }
   },
+
   computed: {
 
       jumlah265: function() {
         return ' Rp. '+this.thousandSeprator(
-          (0 * this.sd1 + 100000 * this.sd2) +
-          (0 * this.smp1 + 100000 * this.smp2) +
-          (0 * this.sma1 + 200000 * this.sma2) 
+          this.r265()
           )  
       },
       jumlah266: function() {
         return ' Rp. '+this.thousandSeprator(
-          (100000 * this.sd1 + 100000 * this.sd2) +
-          (200000 * this.smp1 + 200000 * this.smp2) +
-          (200000 * this.sma1 + 200000 * this.sma2) 
+          (200000 * this.sd1 + 200000 * this.sd2) +
+          (300000 * this.smp1 + 300000 * this.smp2) +
+          (400000 * this.sma1 + 400000 * this.sma2) - this.r265()
           )  
       },
       jumlah268: function() {
@@ -54,6 +52,13 @@ new Vue({
           (100000 * this.sma1 + 100000 * this.sma2) 
           )  
       },
+      jumlah: function() {
+        return ' Rp. '+this.thousandSeprator(
+          (225000 * this.sd1 + 225000* this.sd2) +
+          (375000* this.smp1 + 375000 * this.smp2) +
+          (500000 * this.sma1 + 500000 * this.sma2) 
+        )
+      }
 
   }
 
