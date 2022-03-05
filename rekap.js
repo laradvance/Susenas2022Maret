43dd48329b6a3dcca4a60c6da957099216fb20bf
Laradvance <35951442+laradvance@users.noreply.github.com>
2022-03-05 12:51:47 +0800

    Final 1

--- rekap.js
+++ rekap.js
@@ -47,8 +47,52 @@ new Vue({
       r297:0,
       r304:0,
 
+      r5ak5:0,
+      r5ak6:0,
+      r5ak7:0,
+      r5bk7:0,
+      r5ek2:0,
+      r5ek4:0,
 
+      u1:0,
+      t1a1:0,
+      t1b1:0,
+      t21:0,
+      t31:0,
+      t41:0,
+      t51:0,
+      l1:0,
+      n1:0,
 
+      k2:0,
+      u2:0,
+      t1a2:0,
+      t1b2:0,
+      t22:0,
+      t32:0,
+      t42:0,
+      t52:0,
+      l2:0,
+      n2:0,
+
+      k3:0,
+      u3:0,
+      t1a3:0,
+      t1b3:0,
+      t23:0,
+      t33:0,
+      t43:0,
+      t53:0,
+      l3:0,
+      n3:0,
+
+      d1:0,
+
+      d2:0,
+      d3:0,
+
+      d4:0,
+      d5:0,
 
     };
   },
@@ -99,7 +143,63 @@ new Vue({
 
     setahun() {
       return 12 * this.sebulan()
-    }
+    },
+
+    jumlahu() {
+      return (12 * 30 * this.u1 / 7) + (12 * this.u2) + 1* this.u3
+    },
+    
+    jumlahk() {
+      return (12 * this.k2) + 1* this.k3
+    },
+
+    jumlaht1a() {
+      return (12 * 30 * this.t1a1 / 7) + (12 * this.t1a2) + 1* this.t1a3
+    },
+
+    jumlaht1b() {
+      return (12 * 30 * this.t1b1 / 7) + (12 * this.t1b2) + 1* this.t1b3
+    },
+
+    jumlaht2() {
+      return (12 * 30 * this.t21 / 7) + (12 * this.t22) + 1* this.t23
+    },
+    
+    jumlaht3() {
+      return (12 * 30 * this.t31 / 7) + (12 * this.t32) + 1* this.t33
+    },
+    jumlaht4() {
+      return (12 * 30 * this.t41 / 7) + (12 * this.t42) + 1* this.t43
+    },
+
+    jumlaht5() {
+      return (12 * 30 * this.t51 / 7) + (12 * this.t52) + 1* this.t53
+    },
+
+    jumlahl() {
+      return (12 * 30 * this.l1 / 7) + (12 * this.l2) + 1* this.l3
+    },
+
+    jumlahn() {
+      return (12 * 30 * this.n1 / 7) + (12 * this.n2) + 1* this.n3
+    },
+
+    jumlahb5a() {
+      return (1 * this.r5ak5) + (1 * this.r5ak6) + (1 * this.r5ak7)
+    },
+
+    jumlahb5b() {
+      return (1 * this.r5bk7)
+    },
+
+    jumlahdmasuk() {
+      return (1 * this.d2) + (1 * this.d5) + (1 * this.r5ek2) + (1 * this.jumlahb5a()) + (1 * this.jumlahb5b()) 
+    },
+
+    
+    jumlahdkeluar() {
+      return (1 * this.d1) + (1 * this.d3) + (1 * this.d4) + (1 * this.r5ek4) + (1*this.setahun()-1*this.jumlahn())
+    },
 
   },
 
